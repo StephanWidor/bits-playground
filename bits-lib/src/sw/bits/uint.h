ef24aa3b9a0ebc0fe2108f7ebeef7120cd0dc227
Stephan Widor <stephan@widor.de>
2024-12-07 23:31:38 +0100

    more operators

--- bits-lib/src/sw/bits/uint.h
+++ bits-lib/src/sw/bits/uint.h
@@ -15,6 +15,30 @@ struct uint
 
     bool operator==(const uint &) const = default;
 
+    bool operator<=(const bitset &otherBits) const
+    {
+        for (auto i = bitlength - 1u; i < bitlength; --i)
+        {
+            if (bits[i] != otherBits[i])
+                return otherBits[i];
+        }
+        return true;
+    }
+
+    bool operator<=(const uint &other) const { return operator<=(other.bits); }
+
+    bool operator>=(const bitset &otherBits) const
+    {
+        for (auto i = bitlength - 1u; i < bitlength; --i)
+        {
+            if (bits[i] != otherBits[i])
+                return bits[i];
+        }
+        return true;
+    }
+
+    bool operator>=(const uint &other) const { return operator>=(other.bits); }
+
     uint &operator++()
     {
         for (auto i = 0u; i < bitlength; ++i)
@@ -59,7 +83,7 @@ struct uint
         return copy;
     }
 
-    uint &operator+=(const uint<bitlength> &other)
+    uint &operator+=(const bitset &otherBits)
     {
         uint<2> overflow{0u};
         for (auto i = 0u; i < bitlength; ++i)
@@ -67,20 +91,22 @@ struct uint
             overflow.bits >>= 1;
             if (bits[i])
                 ++overflow;
-            if (other.bits[i])
+            if (otherBits[i])
                 ++overflow;
             bits[i] = overflow.bits[0];
         }
         return *this;
     }
 
-    uint &operator-=(const uint &b)
+    uint &operator+=(const uint<bitlength> &other) { return operator+=(other.bits); }
+
+    uint &operator-=(const bitset &otherBits)
     {
         uint<2> overflow{0u};
         for (auto i = 0u; i < bitlength; ++i)
         {
             overflow.bits >>= 1;
-            if (b.bits[i])
+            if (otherBits[i])
                 ++overflow;
             if (overflow.bits[0] && !bits[i])
                 overflow.bits[1] = true;
@@ -88,6 +114,22 @@ struct uint
         }
         return *this;
     }
+
+    uint &operator-=(const uint &other) { return operator-=(other.bits); }
+
+    uint &operator*=(const bitset &otherBits)
+    {
+        bitset copy{bits};
+        bits.reset();
+        for (auto i = 0u; i < bitlength; ++i)
+        {
+            if (otherBits[i])
+                operator+=(copy << i);
+        }
+        return *this;
+    }
+
+    uint &operator*=(const uint<bitlength> &other) { return operator*=(other.bits); }
 };
 
 template<size_t bitlength>
@@ -100,6 +142,12 @@ template<size_t bitlength>
 uint<bitlength> operator-(const uint<bitlength> &a, const uint<bitlength> &b)
 {
     return uint<bitlength>{a} -= b;
+}
+
+template<size_t bitlength>
+uint<bitlength> operator*(const uint<bitlength> &a, const uint<bitlength> &b)
+{
+    return uint<bitlength>{a} *= b;
 }
 
 }    // namespace sw::bits
--- bits-test/uint.cpp
+++ bits-test/uint.cpp
@@ -6,6 +6,25 @@
 
 namespace sw::bits {
 
+TEST(uint_test, ordering)
+{
+    constexpr size_t bitlength = 16u;
+
+    const auto check = [](unsigned long i0, unsigned long i1) {
+        uint<bitlength> b0{i0};
+        uint<bitlength> b1{i1};
+
+        EXPECT_EQ(b0 <= b1, i0 <= i1);
+        EXPECT_EQ(b0 >= b1, i0 >= i1);
+    };
+
+    std::default_random_engine rand(123);
+    std::uniform_int_distribution<unsigned long> dist(0u, 1u << (bitlength - 1));
+
+    for (auto i = 0; i < 100; ++i)
+        check(dist(rand), dist(rand));
+}
+
 TEST(uint_test, increment)
 {
     constexpr size_t bitlength = 16u;
@@ -37,11 +56,12 @@ TEST(uint_test, addition)
     constexpr size_t bitlength = 32u;
 
     const auto check = [](const unsigned long i0, const unsigned long i1) {
+        const auto sumExpected = i0 + i1;
+
         uint<bitlength> b0{i0};
         uint<bitlength> b1{i1};
 
         const auto sum = (b0 + b1).bits.to_ulong();
-        const auto sumExpected = i0 + i1;
         EXPECT_EQ(sum, sumExpected);
 
         b0 += b1;
@@ -63,12 +83,12 @@ TEST(uint_test, subtraction)
     const auto check = [](unsigned long i0, unsigned long i1) {
         if (i0 < i1)
             std::swap(i0, i1);
+        const auto diffExpected = i0 - i1;
 
         uint<bitlength> b0{i0};
         uint<bitlength> b1{i1};
 
         const auto diff = (b0 - b1).bits.to_ulong();
-        const auto diffExpected = i0 - i1;
         EXPECT_EQ(diff, diffExpected);
 
         b0 -= b1;
@@ -78,6 +98,31 @@ TEST(uint_test, subtraction)
 
     std::default_random_engine rand(12);
     std::uniform_int_distribution<unsigned long> dist(0u, 1u << (bitlength - 1));
+
+    for (auto i = 0; i < 100; ++i)
+        check(dist(rand), dist(rand));
+}
+
+TEST(uint_test, multiplication)
+{
+    constexpr size_t bitlength = 32u;
+
+    const auto check = [](const unsigned long i0, const unsigned long i1) {
+        const auto productExpected = i0 * i1;
+
+        uint<bitlength> b0{i0};
+        uint<bitlength> b1{i1};
+
+        const auto product = (b0 * b1).bits.to_ulong();
+        EXPECT_EQ(product, productExpected);
+
+        b0 *= b1;
+        const auto productInPlace = b0.bits.to_ulong();
+        EXPECT_EQ(productInPlace, productExpected);
+    };
+
+    std::default_random_engine rand(1);
+    std::uniform_int_distribution<unsigned long> dist(0u, 1u << (bitlength / 2u));
 
     for (auto i = 0; i < 100; ++i)
         check(dist(rand), dist(rand));
